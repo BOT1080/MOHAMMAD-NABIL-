9500e484f592440b8902008a97f97ee2381ab6c2
MOHAMMAD NABIL <mohammad2010.biz@gmail.com>
2025-03-24 16:57:29 +0600

    Update antibd.js

--- Priyansh/events/antibd.js
+++ Priyansh/events/antibd.js
@@ -15,6 +15,6 @@ module.exports.run = async function({ api, event, Users, Threads }) {
     if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
         api.changeNickname(nickname, threadID, botID)
         var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} - 𝙏𝙐𝙈 𝘽𝙊𝙏 𝙆𝘼 𝙉𝙄𝘾𝙆𝙉𝘼𝙈𝙀 𝘾𝙃𝘼𝙉𝙂𝙀 𝙉𝙄 𝙆𝘼𝙍 𝙎𝘼𝙆𝙏𝘼𝙔 😹🖐`}, threadID);
+       return api.sendMessage({ body: `${info.name} - 𝙾𝙽𝙻𝚈 𝙾𝚆𝙽𝙴𝚁 𝙼𝙾𝙷𝙰𝙼𝙼𝙰𝙳 𝙽𝙰𝙱𝙸𝙻 𝙲𝙰𝙽 𝙲𝙷𝙰𝙽𝙶𝙴 𝚃𝙷𝙴 𝙽𝙰𝙼𝙴 𝙾𝙵 𝙱𝙾𝚃 ⚠️`}, threadID);
     }  
         }
